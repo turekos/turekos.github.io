bcd20578224ec510ac53679c3f2b6972f53745db
Your Name <you@example.com>
2022-11-30 23:32:08 +0100

    copy button

--- js/app.js
+++ js/app.js
@@ -27,3 +27,11 @@ navSlide();
 /* begin begin Back to Top button  */
 
 /* end begin Back to Top button  */
+
+function copyToClipboard(element) {
+	var $temp = $("<input>");
+	$("body").append($temp);
+	$temp.val($(element).text()).select();
+	document.execCommand("copy");
+	$temp.remove();
+  }
